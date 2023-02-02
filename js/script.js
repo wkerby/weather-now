53c003d327cf4bee041a3c1eb9a205794878a142
William Kerby <w.kerby99@gmail.com>
2023-02-02 11:11:46 -0500

    create js file and begin api call functionality

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//fetch weather data for Atlanta, GA
+myKey = "c55febd7847bed4a6181dc26dc5779cf"
+apiURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+atlantaCoord = [33.7488, 84.3877];
