7e05dc88643a36250c835fc20c84fb8fdd735b7a
William Kerby <w.kerby99@gmail.com>
2023-02-02 13:10:54 -0500

    make getLatLon function that returns lat and lon based off of city name input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 //fetch weather data for Atlanta, GA
-myKey = "c55febd7847bed4a6181dc26dc5779cf";
-atlantaCoord = [33.75, -84.39];
+var myKey = "c55febd7847bed4a6181dc26dc5779cf";
+var atlantaCoord = [33.75, -84.39];
+
+//create function that returns latitued and logitude of an input city
+function getLatLon(key, city) {
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + key;
+    var lat = undefined;
+    var lon = undefined;
+    fetch(requestUrl)
+        .then(function (response) {
+            console.log(response.status)
+            return response.json();
+        })
+        .then(function (data) {
+            var lat = data[0]['lat'];
+            var lon = data[0]['lon'];
+            console.log(lat);
+            console.log(lon);
+            return [lat, lon];
+        });
+}
+
+console.log(getLatLon(myKey, "Atlanta"));
 
 //create function that returns JSON weather data when provided with valid degrees for longitued and latitude
 function getWeather(key, coordinates) {
@@ -13,9 +34,10 @@ function getWeather(key, coordinates) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
+            console.log(data);
         });
 
 
 }
 
+getLatLon(myKey, "Atlanta");
