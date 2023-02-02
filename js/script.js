3b2a6b8e38f5de861e44c846a50307567e539980
William Kerby <w.kerby99@gmail.com>
2023-02-02 11:44:53 -0500

    create function that modifies request url based on parameters passed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 //fetch weather data for Atlanta, GA
-myKey = "c55febd7847bed4a6181dc26dc5779cf"
-apiURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-atlantaCoord = [33.7488, 84.3877];
+myKey = "c55febd7847bed4a6181dc26dc5779cf";
+atlantaCoord = [44.34, 10.99];
+
+function getApi(key, coordinates) {
+    requestUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + coordinates[1].toString() + "&lon="
+        + coordinates[0].toString() + "&appid=" + key;
+
+    // requestUrl = "api.openweathermap.org/data/2.5/forecast?lat=33.75&lon=84.39&appid=c55febd7847bed4a6181dc26dc5779cf"
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+
+
+}
+
+getApi(myKey, atlantaCoord);
+
