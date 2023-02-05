a0498aac1eb2b23527cee0077c0c938b3c286de3
William Kerby <w.kerby99@gmail.com>
2023-02-05 13:09:18 -0500

    finish getWeather function, link with getLatLon function, and begin displaying data on page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+//create variables for elements that will be targeted in document.body
+var currentCityEl = $("#current-city");
+var currentDateEl = $("#current-date");
+var currentTempEl = $("#current-temp");
+var currentWindEl = $("#current-wind");
+var forecastCityEl = $("#forecast-city");
+
 //fetch weather data for Atlanta, GA
 var myKey = "c55febd7847bed4a6181dc26dc5779cf";
 var atlantaCoord = [33.75, -84.39];
@@ -5,8 +12,8 @@ var atlantaCoord = [33.75, -84.39];
 //create function that returns latitued and logitude of an input city
 function getLatLon(key, city) {
     var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + key;
-    var lat = undefined;
-    var lon = undefined;
+    // var lat = undefined;
+    // var lon = undefined;
     fetch(requestUrl)
         .then(function (response) {
             console.log(response.status)
@@ -16,22 +23,20 @@ function getLatLon(key, city) {
             getWeather(myKey, data[0])
             // var lat = (Math.round(data[0]['lat'] * 100) / 100).toString();
             // var lon = (Math.round(data[0]['lon'] * 100) / 100).toString();
-            console.log(lon);
         });
-    return [lat, lon];
 
 }
 
 console.log(getLatLon(myKey, "Atlanta"));
 
 //create function that returns JSON weather data when provided with valid degrees for longitued and latitude
-function getWeather(key, weathData) {
+function getWeather(key, weathData) { //parameter passed in getWeather is the object that is returned from getLatLon
     var { lat, lon, city } = weathData; //use weather
-    // var lat = weathData.lat
-    // var lon = weathData.lon;
-    // var city = weathData.name;
+    var lat = weathData.lat; //round to two decimal places
+    var lon = weathData.lon; //round to two decimal places
+    // var city = weathData.name; do I need city if I am passing as a parameter in getLatLon
 
-    console.log(weathData);
+    // console.log(weathData);
     requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon="
         + lon + "&appid=" + key + "&units=imperial";
 
@@ -42,8 +47,45 @@ function getWeather(key, weathData) {
         })
         .then(function (data) {
             console.log(data);
+            console.log("Forecast for: " + data.list[0].dt_txt);
+            console.log("Temperature: " + data.list[0].main.temp);
+            var city = data.city.name;
+            var date = dateReturn(data.list[0].dt_txt.substring(0, 10).split("-"));
+            currentCityEl.text(city);
+            forecastCityEl.text(city);
+            currentDateEl.text(date);
+
         });
+
+
+
+
+
+
 
 
 }
 
+//create a function to format date from [YYYY,MM,DD] to DD/MM/YYYY
+function dateReturn(dateList) {
+    var dateStr = ["", "", ""];
+
+    for (var i = 0; i < dateList.length; i++) {
+        if (i == 0) {
+            dateStr[2] = dateList[i];
+        }
+
+        else {
+            if (dateList[i] > 1) {
+                dateStr[i - 1] = dateList[i][1];
+            }
+            else {
+                dateStr[i - 1] = dateList[i];
+            }
+        }
+    }
+
+    return dateStr.join("/")
+
+
+}
